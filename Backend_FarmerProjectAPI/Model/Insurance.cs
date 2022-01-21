c3f88ffa7e3ac449770ad9a6f8b91ac382887b07
hars2ha <95631225+hars2ha@users.noreply.github.com>
2022-01-21 22:12:06 +0530

    frist commit

---
+++ Backend_FarmerProjectAPI/Model/Insurance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_FarmerProjectAPI.Model
+{
+    public class Insurance
+    {
+        [Key]
+        public int I_Id { get; set; }
+        public string season { get; set; }
+        public int year { get; set; }
+        public string crop { get; set; }
+        public int area { get; set; }
+        public int sum_Insured { get; set; }
+        public int share_Premium { get; set; }
+        public int premium_Amount { get; set; }
+        public int total_sum { get; set; }
+
+        [ForeignKey("Insurance_Company")]
+        public string Insurance_Company { get; set; }
+        
+
+    }
+}
---
+++ Backend_FarmerProjectAPI/Model/Sell_Request.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_FarmerProjectAPI.Model
+{
+    public class Sell_Request
+    {
+        [Key]
+        public int s_ID { get; set; }
+        public string crop_Name { get; set; }
+
+        [ForeignKey("crop_Type")]
+        public string crop_Type { get; set; }
+        public string fertilizer_type { get; set; }
+        public int soil_ph { get; set; }
+        public int quantity { get; set; }
+
+    }
+}
